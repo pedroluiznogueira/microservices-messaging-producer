5b903bb3eb9375f5ccfe9da19a16a011ca92774b
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-22 15:10:23 -0300

    Rabbit template used methods to send any kind of data, we can send a simple string

--- src/main/java/com/github/pedroluiznogueira/microservices/messaging/MessagingApplication.java
+++ src/main/java/com/github/pedroluiznogueira/microservices/messaging/MessagingApplication.java
@@ -18,6 +18,6 @@ public class MessagingApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		rabbitTemplate.convertAndSend("First message!");
 	}
 }
