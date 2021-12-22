7503c6460171835e3b3214372687fc89dcb13fb4
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-22 15:09:30 -0300

    Setted up project to work with rabbitmq

--- src/main/java/com/github/pedroluiznogueira/microservices/messaging/MessagingApplication.java
+++ src/main/java/com/github/pedroluiznogueira/microservices/messaging/MessagingApplication.java
@@ -1,13 +1,23 @@
 package com.github.pedroluiznogueira.microservices.messaging;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MessagingApplication {
+public class MessagingApplication implements CommandLineRunner {
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
