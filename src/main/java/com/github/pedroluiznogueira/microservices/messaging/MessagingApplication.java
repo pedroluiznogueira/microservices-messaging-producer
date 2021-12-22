88657237d9c4c4aeb71efb60c75f0a49ebee5393
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-22 15:05:05 -0300

    Initial commit

---
+++ src/main/java/com/github/pedroluiznogueira/microservices/messaging/MessagingApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedroluiznogueira.microservices.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingApplication.class, args);
+	}
+
+}
